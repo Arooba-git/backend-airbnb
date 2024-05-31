d65cf1d53bb2861008a1d1885528ca5b532a23c2
Aroooba <arooba.shahoor@gmail.com>
2024-05-31 14:36:18 +0900

    update allowed hosts

--- djangobnb_backend/settings.py
+++ djangobnb_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "frontend-airbnb-nextjs-production.up.railway.app"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "frontend-airbnb-nextjs-production.up.railway.app", "0.0.0.0"]
 
 AUTH_USER_MODEL = 'useraccount.User'
 
