d602b673589be2a86e3aae76701c6eb73310011f
Aroooba <arooba.shahoor@gmail.com>
2024-06-04 10:29:03 +0900

    fix uuid

--- property/models.py
+++ property/models.py
@@ -6,7 +6,8 @@ from django.db import models
 from useraccount.models import User
 
 class Property(models.Model):
-    id=models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
     title=models.CharField(max_length=255)
     description=models.TextField()
     price_per_night=models.IntegerField()
